f704296f4eedd4fd9880393cf7f1fd6fafea94b8
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-10-28 21:44:14 +0530

    taripan asach

---
+++ javaf.java
@@ -0,0 +1,109 @@
+// public class JavaBasics {
+//     public static void main (String args[]){
+//         System.out.println("Hello World");
+//                 System.out.print("Hello World");
+
+//         System.out.println("Hello World");
+
+//     }
+// }
+
+
+// public class javaf {
+//     public static void main (String args[]){
+//         System.out.println("****");
+//         System.out.println("***");
+//         System.out.println("**");
+//         System.out.println("*");
+//     }
+// }
+
+
+// public class javaf {
+//     public static void main (String args[]){
+//         int a = 10;
+//         int b = 5;
+//         String name = "Kohli";
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(name);
+
+//         a = 50;
+//         System.out.println(a);
+//     }
+// }
+
+
+// public class javaf {
+//     public static void main (String args[]){
+//         byte b = 4;
+//         System.out.println(b);
+//         char ch = 'a';
+//         System.out.println(ch);
+//         boolean var = false;
+//         float price = 10.5;
+//         int number = 25;
+//         //long
+//         //double
+//         short n =240;
+//     }
+// }
+
+
+// public class javaf {
+//     public static void main (String args[]){
+//         int a =10;
+//         int b=5;
+//         int sum = a+b;
+//         System.out.println(sum);
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class javaf {
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         // String input = sc.next();
+//         // System.out.println(input);
+
+//         // String name = sc.nextLine();
+//         // System.out.println(name);
+
+
+//         // int number = sc.nextInt();
+//         // System.out.println(number);
+
+
+//         // float number = sc.nextFloat();
+//         // System.out.println(number);
+
+//     }
+// }
+
+
+// import java.util.*;
+// public class javaf {
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a+b;
+
+//         System.out.println(sum);
+//     }
+// }
+
+// import java.util.*;
+// public class javaf {
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int product = a*b;
+
+//         System.out.println(product);
+//     }
+// }
+
