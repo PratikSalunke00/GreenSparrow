7aaa92586f284bbae7c433422a8bfe90fe0d4528
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-11-03 19:56:44 +0530

    DSA

--- javaf.java
+++ javaf.java
@@ -385,21 +385,37 @@
 // }
 
 
+// import java.util.*;
+// public class javaf {
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int income = sc.nextInt();
+//         int tax;
+
+//         if (income < 500000) {
+//             tax = 0;
+//         } else if (income >= 500000 && income<1000000) {
+//             tax = (int) (income * 0.2);
+//         } else {
+//             tax = (int) (income * 0.3);
+//         }
+//         System.out.println("your tax is:" + tax);
+//     }
+// }
+
 
 import java.util.*;
 public class javaf {
     public static void main (String args[]){
-        Scanner sc = new Scanner(System.in);
-        int income = sc.nextInt();
-        int tax;
-
-        if (income < 500000) {
-            tax = 0;
-        } else if (income >= 500000 && income<1000000) {
-            tax = (int) (income * 0.2);
-        } else {
-            tax = (int) (income * 0.3);
-        }
-        System.out.println("your tax is:" + tax);
+    int a = 1;
+    int b =8;
+    int c =6;
+    if ((a>=b) && (a>=c)) {
+        System.out.println("a is large");
+    } else if ((b>a) && (b>c)) {
+        System.out.println("b is large");
+    } else {
+        System.out.println("c is large");
+    }
     }
 }
