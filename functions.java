ffcafdda1249a877d9ef9c05ba9713aa8d2fa256
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-11-19 13:21:40 +0530

    Functions

--- functions.java
+++ functions.java
@@ -117,7 +117,7 @@ import java.util.*;
 public class functions {
     public static int factorial(int n){
         int f = 1;
-        
+
         for( int i= 1; i<=n; i++) {
             f = f*i;
         }
@@ -136,6 +136,124 @@ public class functions {
 
     public static void main(String args[]) {
         System.out.println(binoCoe(5 ,2));
+    } 
+}
+*/
+
+
+// **** To find the sum of numbers using functions(using parameters) ****
+/*
+import java.util.*;
+public class functions {
+    public static int sum(int a , int b) {
+        return a+b;
+    }
+    
+    public static int sum(int a , int b , int c) {
+        return a+b+c;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(sum(3,5));
+        System.out.println(sum(3,5,6));
+    } 
+}
+*/
+
+
+// **** To find the sum of numbers using functions(using data types) ****
+/*
+import java.util.*;
+public class functions {
+    public static int sum(int a , int b) {
+        return a+b;
+    }
+
+    public static float sum(float a , float b) {
+        return a+b;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(sum(3,5));
+        System.out.println(sum(3.6f , 7.8f));
+    } 
+}
+*/
+
+
+// **** To check if a number is Prime or not ****
+/*
+import java.util.*;
+public class functions {
+    public static boolean isPrime(int n){
+        for( int i=2; i<=n-1; i++) {
+           if(n % == 0) {
+            return false;
+           }
+        }
+
+        return true;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(isPrime(5));
+    } 
+}
+*/
+
+
+// **** To check if a number is Prime or not (using optimized approach) ****
+/*
+import java.util.*;
+public class functions {
+    public static boolean isPrime(int n) {
+        if(n==2) {
+            return true;
+        }
+        for (int i=2; i<=Math.sqrt(n); i++) {
+            if(n%i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(isPrime(7));
+    } 
+}
+*/
+
+
+// **** To check if a number is Prime or not (using optimized approach) ****
+/*
+import java.util.*;
+public class functions {
+    public static boolean isPrime(int n) {
+        if(n==2) {
+            return true;
+        }
+        for (int i=2; i<=Math.sqrt(n); i++) {
+            if(n%i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void primesInRange(int n) {
+        for(int i=2; i<=n; i++) {
+            if(isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        primesInRange(20);
     } 
 }
 */
