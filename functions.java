456318af93e94f8a439e0d66e1884f09e0be200e
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-11-19 23:20:53 +0530

    pattern printing

--- functions.java
+++ functions.java
@@ -311,3 +311,34 @@ public class functions {
     } 
 }
 */
+
+
+// **** FUNCTIONS QUESTIONS ****
+
+// Question 1 :Write a Java method to compute the averageof three numbers.
+/*
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input the first number: ");
+        double x = sc.nextDouble();
+
+        System.out.print("Input the second number: ");
+        double y = sc.nextDouble();
+
+        System.out.print("Input the third number: ");
+        double z = sc.nextDouble();
+
+        System.out.println("The average value is " + average(x, y, z));
+    }
+
+    public static double average(double x, double y, double z) {
+        return (x + y + z) / 3;
+    }
+}
+*/
+
+//
